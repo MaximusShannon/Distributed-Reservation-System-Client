5d4e8ae566d36051ea1eaff87f84efd4106fba4d
Max Shannon <Maxshannon2016@hotmail.com>
2018-02-05 21:50:58 +0000

    Sending client request to server

---
+++ src/com/maxshannon/controllers/LoginController.java
@@ -0,0 +1,40 @@
+package com.maxshannon.controllers;
+
+import com.maxshannon.functionality.ClientServerConnection;
+import com.maxshannon.functionality.LoginRequest;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    private String username;
+    private String password;
+    private LoginRequest loginRequest;
+    private ClientServerConnection clientServerConnection;
+
+    @FXML
+    private TextField usernameTextField;
+    @FXML
+    private TextField passwordTextField;
+    @FXML
+    private Button loginButton;
+
+    @FXML
+    public void getLoginDetails(){
+
+        clientServerConnection = new ClientServerConnection();
+
+        username = usernameTextField.getText();
+        password = passwordTextField.getText();
+
+        loginRequest = new LoginRequest(username, password);
+        System.out.println("Sending Login Request");
+        clientServerConnection.startClientLoginRequest(loginRequest);
+
+
+    }
+
+
+
+}
