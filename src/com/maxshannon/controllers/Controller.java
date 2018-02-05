0ea93575d54c53159eb03b31e0f3e6129b202750
Max Shannon <Maxshannon2016@hotmail.com>
2018-02-05 20:12:24 +0000

    Initial Login View

---
+++ src/com/maxshannon/controllers/Controller.java
@@ -0,0 +1,6 @@
+package com.maxshannon.controllers;
+
+public class Controller {
+
+
+}
--- src/com/maxshannon/functionality/Main.java
+++ src/com/maxshannon/functionality/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package com.maxshannon.functionality;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
